66ff25b3413f584e32d630fc314b7abdaca9a7d8
asd <asd@asd.bg>
2018-01-10 12:49:23 +0200

    da wae to ughanda

--- spec/controllers/rsas_controller_spec.rb
+++ spec/controllers/rsas_controller_spec.rb
@@ -2,8 +2,7 @@ RSpec.describe RsasController do
 
 	it "gets by id" do
 		get :show, params: {id: 1}
-		expect(response.body).to eq 
-			"the content"
+		expect(response.body).to eq "the content"
 	end
 
 end
